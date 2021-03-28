e48d79bdbf169f101d33703cf6f26d2448a09d9c
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-28 14:39:52 -0400

    72: store unsubscribe shorthand

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,20 +1,10 @@
 <script>
-  // LIFECYCLES
-  import { onMount, onDestroy } from "svelte";
   // STORE
   import products from "../../stores/defaultProducts";
-  // VARIABLES
-  let localProducts = [];
-  onMount(() => {
-    products.subscribe((value) => {
-      console.log(value);
-      localProducts = value;
-    });
-  });
 </script>
 
 <h1>hello from products component</h1>
 
-{#each localProducts as product}
+{#each $products as product}
   <h2>{product.title}</h2>
 {/each}
