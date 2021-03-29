239b1d61fa3a7d0a2848ddcf58a163007eaa2742
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-29 10:11:18 -0400

    77: derived stores

--- src/components/Products/Featured.svelte
+++ src/components/Products/Featured.svelte
@@ -1,21 +1,20 @@
 <script>
   // STORE
-  import products from "../../stores/defaultProducts";
+  import products, { featuredStore } from "../../stores/defaultProducts";
   // COMPONENTS
   import Product from "./Product.svelte";
   import Loading from "../Loading.svelte";
   // VARIABLES
   export let title = "";
-  $: featured = $products.filter((item) => item.featured === true);
 </script>
 
-{#if featured.length === 0}
+{#if $featuredStore.length === 0}
   <Loading />
 {:else}
   <section class="section">
     <h2 class="section-title">{title}</h2>
     <div class="products-center">
-      {#each featured as product (product.id)}
+      {#each $featuredStore as product (product.id)}
         <Product {product} />
       {/each}
     </div>
