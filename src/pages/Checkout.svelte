ee63020f20845a03b525a3892cee27da47ab8e66
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-31 12:25:59 -0400

    133: checkout form - basic setup

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -11,10 +11,40 @@
       navigate("/");
     }
   });
+  // FUNCTIONS
+  function handleSubmit() {
+    console.log("form submitted");
+  }
+  // VARIABLES
+  let name = "";
+  $: isEmpty = !name;
 </script>
 
 {#if $cartTotal > 0}
-  <h1>form</h1>
+  <section class="form">
+    <h2 class="section-title">checkout</h2>
+    <form class="checkout-form" on:submit|preventDefault={handleSubmit}>
+      <h3>order total : ${$cartTotal}</h3>
+      <!-- single input -->
+      <div class="form-control">
+        <label for="name">your name</label>
+        <input type="text" id="name" bind:value={name} />
+      </div>
+      <!-- end of single input -->
+      <!-- stripe stuff -->
+      <!-- end of stripe stuff -->
+      <!-- error message -->
+      {#if isEmpty}
+        <p class="form-empty">please fill out name field</p>
+      {/if}
+      <!-- submit button -->
+      <button
+        class="btn btn-block btn-primary"
+        type="submit"
+        disabled={isEmpty}
+        class:disabled={isEmpty}>submit</button>
+    </form>
+  </section>
 {:else}
   <div class="checkout-empty">
     <h2>your cart is empty</h2>
