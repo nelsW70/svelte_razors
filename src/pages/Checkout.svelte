aa7edeeeaa2b61ea75c85660bde616b598e0df18
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-31 10:59:46 -0400

    130: checkout page basics

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -1,1 +1,14 @@
+<script>
+  // MODULES
+  import { onMount } from "svelte";
+  import { navigate, link } from "svelte-routing";
+  // STORES
+  import user from "../stores/user";
+  import { cartTotal } from "../stores/cart";
+</script>
+
+<h1>welcome to checkout page</h1>
+<h1>welcome to checkout page</h1>
+<h1>welcome to checkout page</h1>
+<h1>welcome to checkout page</h1>
 <h1>welcome to checkout page</h1>
