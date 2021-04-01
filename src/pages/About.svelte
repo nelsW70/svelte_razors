c0cf14c77e7611a89619840c63cb9652cdecd208
Nelson Foltz <nelsfoltz@gmail.com>
2021-04-01 15:01:47 -0400

    add: about section styling/content

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,1 +1,11 @@
-<h1>welcome to about page</h1>
+<section class="section about-section">
+  <h1 class="section-title">about us</h1>
+  <p>
+    Hoodie offal waistcoat, 8-bit locavore pork belly echo park celiac pour-over
+    synth scenester live-edge vegan listicle slow-carb. Cred truffaut organic
+    poutine, woke locavore mustache brooklyn shaman brunch sriracha cloud bread.
+    Gluten-free vaporware viral four dollar toast retro hexagon VHS vinyl green
+    juice echo park woke. Wolf street art etsy man braid dreamcatcher kale chips
+    microdosing truffaut la croix brunch woke.
+  </p>
+</section>
